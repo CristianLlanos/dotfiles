e5cb5c27656441694aee81bedcc12a2977a2c9b8
Cristian Llanos <cristian@cinepapaya.com>
2016-06-24 22:39:54 -0500

    Add installation rules

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 #=============================================================================#
 # Title       : install.sh
 # Description : Sets up my configuration files
@@ -7,5 +7,59 @@
 # Usage       : bash install.sh
 #=============================================================================#
 
+SOURCE=${BASH_SOURCE[0]}
+DOTFILES=$(cd $(dirname ${SOURCE}) && pwd)
+
+# If the user needs sudo privileges, proxy the command using sudo.
+if [[ "$EUID" -ne 0 ]]
+then
+    sudo ${SOURCE} "$@"
+    exit
+fi
+
+#==============================================================================
+# Git
+#==============================================================================
+
+ln -s ${DOTFILES}/.gitconfig ~/.gitconfig
+ln -s ${DOTFILES}/.gitignore-global ~/.gitignore-global
+echo "Git [configurations] registered"
+
+#==============================================================================
+# Vim
+#==============================================================================
+
+# Vim resource file
+ln -s ${DOTFILES}/.vimrc ~/.vimrc
+echo "Vim [vimrc] registered"
+
+# Vim Vundle plugins and configuration
+ln -s ${DOTFILES}/plugins.vimrc ~/plugins.vimrc
+echo "Vim [vundle plugins] registered"
+
+# Vim colors
+mkdir -p ~/.vim/colors
+ln -s ${DOTFILES}/vim/colors ~/.vim/colors
+echo "Vim [colors] registered"
+
+# Vim snippets
+mkdir -p ~/.vim/snippets
+ln -s ${DOTFILES}/vim/snippets ~/.vim/snippets
+echo "Vim [snippets] registered"
+
+#==============================================================================
+# Fish
+#==============================================================================
+
+mkdir -p ~/.config/fish
+ln -s ${DOTFILES}/config.fish ~/.config/fish/config.fish
+echo "Fish [configurations] registered"
+
+#==============================================================================
+# Binaries
+#==============================================================================
+
 # Enables vhost command
-sudo cp ./scripts/vhost.sh /usr/local/bin/vhost
+cp ${DOTFILES}/scripts/vhost.sh /usr/local/bin/vhost
+sudo chmod a+x /usr/local/bin/vhost
+echo "Command [vhost] enabled"
