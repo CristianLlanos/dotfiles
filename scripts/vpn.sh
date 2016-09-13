8f2bce5f4fe00143cce5f059b2ed0061a2a2c3c2
Cristian Llanos <cristian@cinepapaya.com>
2016-09-13 10:03:54 -0500

    Add VPN script

---
+++ scripts/vpn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=============================================================================#
+# Title       : vpn.sh
+# Description : Registers an Apache2 Virtual Host.
+# Author      : Cristian Llanos <cristianllanos@outlook.com>
+# Date        : 2016-09-09T18:40:00Z-05:00
+# Usage       : vpn [connection]
+#=============================================================================#
+
+SOURCE=${BASH_SOURCE[0]}
+
+# If the user needs sudo privileges, proxy the command using sudo.
+if [[ "$EUID" -ne 0 ]]
+then
+    sudo ${SOURCE} "$@"
+    exit
+fi
+
+openvpn --config ~/vpn/${1}.ovpn
