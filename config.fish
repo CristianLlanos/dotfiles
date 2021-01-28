5ec327fb604e7eb2560e71214aea5a323e831413
Cristian Llanos <hello@cristianllanos.com>
2021-01-27 22:03:26 -0500

    Update configurations

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
+set fish_greeting ''
+
+set PATH ./node_modules/.bin $PATH
 set PATH ./vendor/bin $PATH
 set PATH ~/.config/composer/vendor/bin $PATH
 
-alias artisan="php artisan"
-alias tinker="php artisan tinker"
-alias serve="php -S localhost:8000"
+alias du="UID_GID=(id -u):(id -g) docker-compose -f ~/code/talently/docker-compose.yml up -d"
+alias ds="docker-compose -f ~/code/talently/docker-compose.yml stop"
